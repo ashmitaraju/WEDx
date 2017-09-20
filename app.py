c388ee425dc3cb4733f186e19b8a83a089d6bca6
Aravind B.S <bs.aravind.97@gmail.com>
2017-09-21 01:06:44 +0530

    sign up ui updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, json
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,13 +8,24 @@ def main():
 @app.route('/showSignUp')
 def showSignUp():
     return render_template('signup.html')
-
+@app.route('/signUp', methods=['POST'])
 def signUp():
-	_name = request.form['inputName']
-	_email = request.form['inputEmail']
-	_password = request['inputPassword']
+	#read values posted in the UI
+	_username = request.form['inputUsername']
+	_fname = request.form['inputFName']
+	_lname = request.form['inputLName']
+	_password = request.form['inputPassword']
 
-	
+	#validate the receieved values
+
+	if _username and _fname and _lname and _password:
+		return json.dumps({'html':'<span>All fields good !!</span>'})
+
+	else:
+		 return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+
+
 
 if __name__=="__main__":
 	app.run()
