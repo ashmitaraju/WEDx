df162743a28cf9b685929c62a7ab10362e2f1ae4
Aravind B.S <bs.aravind.97@gmail.com>
2017-09-22 02:15:47 +0530

    updated database queries. jQuery error still persists

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 from flask import Flask, render_template, request, json
+from flask.ext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
+
+
 app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'wedx'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+
 
 @app.route("/")
 def main():
@@ -10,19 +22,40 @@ def showSignUp():
     return render_template('signup.html')
 @app.route('/signUp', methods=['POST'])
 def signUp():
+	try:
 	#read values posted in the UI
-	_username = request.form['inputUsername']
-	_fname = request.form['inputFName']
-	_lname = request.form['inputLName']
-	_password = request.form['inputPassword']
+		_username = request.form['inputUsername']
+		_fname = request.form['inputFName']
+		_lname = request.form['inputLName']
+		_password = request.form['inputPassword']
 
-	#validate the receieved values
+		#validate the receieved values
 
-	if _username and _fname and _lname and _password:
-		return json.dumps({'html':'<span>All fields good !!</span>'})
+		if _username and _fname and _lname and _password:
 
-	else:
-		 return json.dumps({'html':'<span>Enter the required fields</span>'})
+			conn = mysql.conect()
+			cursor = conn.cursor()
+			_hashed_password = generate_password_hash(_password)
+			cursor.callproc('sp_createUser', (_fname, _lname, _username, _hashed_password))
+			data = cursor.fetchall()
+
+			if len(data) is 0:
+				conn.commit()
+				return json.dumps({'message':"User created successfully"})
+
+			else:
+				return json.dumps({'error': str(data[0])})
+
+		else:
+			return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+	except Exception as e:
+		return json.dumps({'error':str(e)})
+
+	finally:
+		cursor.close()
+		conn.close()
+
 
 
 
