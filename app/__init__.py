57a0fd39e816e9de4503b6e884cc0144efa8e6af
Aravind B.S <bs.aravind.97@gmail.com>
2017-10-12 16:59:27 +0530

    new branch

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+
 from app import views
--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'wedx-secret-key'
+SQLALCHEMY_DATABASE_URI = 'mysql://aravindbs:aravindbs@localhost/wedx'
