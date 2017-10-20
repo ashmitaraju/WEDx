a275061bef5e7f67a53f0d78c0b81a1c5063238d
Aravind B.S <bs.aravind.97@gmail.com>
2017-10-21 03:16:15 +0530

    db string

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'wedx-secret-key'
-SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@localhost:3306/wedx'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:aravindbs@localhost:3306/wedx'
