1c8b7da9040a5237a7ec217aee3b95c70213c3b0
Aravind B.S <bs.aravind.97@gmail.com>
2017-10-23 10:03:08 +0530

    changes

---
+++ config.py
@@ -0,0 +1,13 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'wedx-secret-key'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:aravindbs@localhost:3306/wedx'
+UPLOADS_DEFAULT_DEST = 'app/static/img/'
+UPLOADS_DEFAULT_URL = '../static/img/'
+
+UPLOADED_IMAGES_DEST ='app/static/img/'
+UPLOADED_IMAGES_URL = '../static/img/'
+#Go sleep
+#goodnight 
+#dream about jesus.
+#aye aye captain 
+# #bsout
