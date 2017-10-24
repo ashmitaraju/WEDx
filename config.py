f3854e4936d9e90e5af4c710fe3190f4e6aa647f
Aravind B.S <bs.aravind.97@gmail.com>
2017-10-24 14:46:46 +0530

    few changes

--- config.py
+++ config.py
@@ -1,23 +1,9 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'wedx-secret-key'
 SQLALCHEMY_DATABASE_URI = 'mysql://root:aravindbs@localhost:3306/wedx'
-SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@localhost:3306/wedx'
+#SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@localhost:3306/wedx'
 UPLOADS_DEFAULT_DEST = 'app/static/img/'
 UPLOADS_DEFAULT_URL = '../static/img/'
 
 UPLOADED_IMAGES_DEST ='app/static/img/'
 UPLOADED_IMAGES_URL = '../static/img/'
-#Go sleep
-#goodnight 
-#dream about jesus.
-#aye aye captain 
-# #bsout
-#goodnight
-#dream about jesus.
-#aye aye captain
-# #bsout
-
-#what did you do
-#thu. it cant even show errors properly
-#kthnxbai
-#bye
